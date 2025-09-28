6fb54d0aaec3547821b7ad61f41b2d885482b6cb
pharland <110207014+pharland@users.noreply.github.com>
2025-09-28 23:48:00 +1000

    Added upgrade for multiplying dosh per email

---
+++ Assets/Scripts/Upgrades/UpgradeBonusPay.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>
+/// This upgrade increases dosh received after every successful email
+/// </summary>
+public class UpgradeBonusPay : MonoBehaviour, IUpgradeBehaviour
+{
+    StatsManager statsManager;
+
+    public void Awake()
+    {
+        statsManager = GameObject.Find("StatsManager").GetComponent<StatsManager>();
+    }
+
+    public void ActivateUpgrade()
+    {
+        statsManager.doshModifier += 0.3f;
+    }
+
+    public void DeactivateUpgrade()
+    {
+        statsManager.doshModifier = 1f;
+    }
+
+    public void IncreaseUpgradeLevel()
+    {
+        //increase dosh modifier by 0.3f per level, then 80% of that increase for each subsequent level
+        statsManager.doshModifier += 0.3f * 0.8f;
+    }
+}
